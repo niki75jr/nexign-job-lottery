4c08d44e08820eb74d5958d1de6671d28d9f7559
Roman Nikitin <n75rom@gmail.com>
2022-06-29 23:54:19 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS participants
 (
     id         BIGSERIAL PRIMARY KEY,
-    first_name varchar(50),
-    last_name  varchar(50),
-    age        int,
-    city       varchar(50)
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    age        INT         NOT NULL,
+    city       VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS winner_records
 (
-    id             bigserial PRIMARY KEY,
-    participant_id bigint REFERENCES participants (id),
-    sum            int,
-    created_at     timestamptz
+    id             BIGSERIAL PRIMARY KEY,
+    participant_id BIGINT REFERENCES participants (id) NOT NULL,
+    sum            INT                                 NOT NULL,
+    created_at     timestamptz                         NOT NULL
 )
