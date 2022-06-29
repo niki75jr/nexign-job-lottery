71e5aec2a531c015f559f3ae85a3be9bcc91432e
Roman Nikitin <48251545+niki75jr@users.noreply.github.com>
2022-06-29 21:38:04 +0900

    Add LotteryApp

---
+++ src/main/java/com/n75jr/nexign/lottery/LotteryApp.java
@@ -0,0 +1,12 @@
+package com.n75jr.nexign.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotteryApp.class, args);
+	}
+}
