c4b3e4802c2992f40496dead44429005cb2df5aa
Roman Nikitin <n75rom@gmail.com>
2022-06-30 10:01:56 +0900

    Transform RandomParticipantMapper to a static class

--- src/main/java/com/n75jr/nexign/lottery/mapper/RandomParticipantMapper.java
+++ src/main/java/com/n75jr/nexign/lottery/mapper/RandomParticipantMapper.java
@@ -3,13 +3,11 @@ package com.n75jr.nexign.lottery.mapper;
 import com.n75jr.nexign.lottery.domain.Participant;
 import com.n75jr.nexign.lottery.dto.RandomParticipantDTO;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.stereotype.Component;
 
 @Log4j2
-@Component
 public class RandomParticipantMapper {
 
-    public Participant toParticipant(RandomParticipantDTO dto) {
+    public static Participant toParticipant(RandomParticipantDTO dto) {
         if (log.isTraceEnabled()) {
             log.trace("Mapping DTO to participant");
         }
@@ -22,7 +20,7 @@ public class RandomParticipantMapper {
         );
     }
 
-    public RandomParticipantDTO toRandomParticipantDTO(Participant participant) {
+    public static RandomParticipantDTO toRandomParticipantDTO(Participant participant) {
         if (log.isTraceEnabled()) {
             log.trace("Mapping participant to DTO");
         }
